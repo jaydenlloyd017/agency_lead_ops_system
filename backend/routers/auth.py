d4cda1bb801a45f546cfdf6483ebeb2186821d71
Jayden Lloyd <jaydenlloyd017@gmail.com>
2026-01-01 14:01:48 +0000

    function to decode jwt complete

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -7,14 +7,14 @@ from backend.schemas import LeadCreate, LeadStatusUpdate, UserCreate, UserRespon
 from backend.jwt_utils import create_access_token
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
-from jose import jwt
+from jose import jwt, JWTError
 from datetime import datetime, timedelta, timezone
 from backend.database import get_db
 
 router = APIRouter()
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-oauth2_bearer = OAuth2PasswordBearer(tokenURL=Token)
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
 
 SECRET_KEY = "55e4e9549904a7ac222690c23793855e906db6dc6fca802c5f5b8ad78deaa679" 
 ALGORITHM = "HS256"
@@ -60,6 +60,19 @@ def create_access_token(email: str, user_id: int, expires_delta: timedelta):
     expires = datetime.now(timezone.utc) + expires_delta
     encode.update({'exp': expires})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        if email is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORISED,
+                                 detail='Could not validate user.')
+        return {'email': email, 'id': user_id}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORISED,
+                             detail='Could not validate user.')
 
 
 
